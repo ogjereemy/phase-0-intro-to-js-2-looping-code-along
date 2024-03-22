3cad96fb9c35233bba82f920af18daf48444661e
ogola <ogolajeremy601@gmail.com>
2024-03-22 17:48:01 +0300

    adds-loop-lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function countDown(){
+    for(let i =10; i >= 0; i--){
+        console.log(i)
+    }
+}
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names) {
+    let thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+        thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return thankYouMessages;
+}
+
+
